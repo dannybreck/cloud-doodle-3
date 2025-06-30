320217a5df4dbe84d379315a6ce949e84c849cb0
dannybreck <142409809+dannybreck@users.noreply.github.com>
2025-06-29 20:21:40 -0400

    Updated _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,12 @@
-import { useEffect } from 'react';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import { useFrameworkReady } from '@/hooks/useFrameworkReady';
 
 export default function RootLayout() {
-  useFrameworkReady();
-
   return (
     <>
       <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="editor" options={{ headerShown: false }} />
+        <Stack.Screen name="(tabs)" />
+        <Stack.Screen name="editor" />
         <Stack.Screen name="+not-found" />
       </Stack>
       <StatusBar style="auto" />
